1694b24cca48f9befeb92a9237552d4cf284fb08
daavidwlf <wolf_david@gmx.de>
2024-08-23 12:11:08 +0200

    fix: env variables not loading

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
-	//change to env later
-	port := "3000"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port := os.Getenv("BACKEND_PORT")
 
 	server := createServer(":" + port)
 	connectDB()
